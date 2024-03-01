8439c643fc079455f0c8219af4a68e905a78b333
MZubair <muhammadzubair78692110@gmail.com>
2024-03-01 23:32:03 +0500

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,25 @@
+// console.log("This is to check the Linkage Between HTML and JavaScript Files.");
+// const hamgurger = document.querySelector('.hamburger');
+// const btn_1 = document.querySelector('.btn-1');
+// const btn_2 = document.querySelector('.btn-2');
+// const btn_3 = document.querySelector('.btn-3');
+// console.log(btn_3);
+
+// btn_1.addEventListener('click', function a() {
+//     const a1 = document.querySelector('.w1');
+//     a1.style.transform = `translateX(-100%)`
+//     alert("Helo")
+// });
+// btn_2.addEventListener('click', function a() {
+//     const a2 = document.querySelector('.w2');
+//     alert("Helo")
+// });
+// btn_3.addEventListener('click', function a() {
+//     const a3 = document.querySelector('.w3');
+
+//     alert("Helo")
+// });
+// const 
+$('.slider-box').slick({
+    rtl: true
+});
